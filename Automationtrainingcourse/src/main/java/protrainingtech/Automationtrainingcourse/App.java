24cd3cbd1f08b3d5037d711f7e85c918193cd40a
faiza <faiza@LAPTOP-ROO3UN56>
2021-12-06 19:52:15 -0500

    course1

---
+++ Automationtrainingcourse/src/main/java/protrainingtech/Automationtrainingcourse/App.java
@@ -0,0 +1,13 @@
+package protrainingtech.Automationtrainingcourse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
